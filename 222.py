1bdaa8a1211c1cf31766db8a87e3a8c0da6c0ceb
TzuHan1209 <78334883+TzuHan1209@users.noreply.github.com>
2021-02-01 17:10:32 +0800

    Add files via upload

---
+++ 222.py
@@ -0,0 +1,11 @@
+score=int(input('what is your score'))
+if score >= 90:
+    print('a')
+elif score >= 80:
+    print('b')
+elif score >= 70:
+    print('c')
+elif score >= 60:
+    print('d')    
+else:
+    print('e')
