a57a946722a78d6451cdd1a9ec5a5f26cdc1bbe6
TzuHan1209 <78334883+TzuHan1209@users.noreply.github.com>
2021-02-02 09:11:54 +0800

    Add files via upload

--- score.py
+++ score.py
@@ -1,11 +1,11 @@
-score=int(input('what is your score'))
-if score >= 90:
-    print('a')
-elif score >= 80:
-    print('b')
-elif score >= 70:
-    print('c')
-elif score >= 60:
-    print('d')    
-else:
-    print('e')
+math=int(input('score'))
+eng=int(input('score'))
+if math >= 90 and eng >= 90 :
+    print('you get a prize' )
+elif math < 60 and eng < 60 :
+    print('要處罰')
+elif math < 60 or eng < 60 :
+    print('加油')
+elif math >= 60 and eng >= 60 :
+    print('???')
+    
