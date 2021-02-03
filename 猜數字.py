7124a5281b85b4f28892db1ec414631e7660ed38
TzuHan1209 <78334883+TzuHan1209@users.noreply.github.com>
2021-02-03 08:42:31 +0800

    Add files via upload

---
+++ 猜數字.py
@@ -0,0 +1,25 @@
+import random
+a=int(input('最大'))
+b=int(input('最小'))
+c=int(input('幾次機會'))
+num=random.randint(b,a)
+number = int(input('number'))
+if number != num :
+    for i in range(1,c):
+        while number > a or number < b :
+            print('輸入錯誤')
+            number = int(input('number'))
+        if num > number:
+            print('太小')
+        elif num == number :
+            break
+        else:
+            print('太大')
+        print('you are wrong')
+        c=c-1
+        print('你還有',c,'次')
+        number = int(input('number'))
+print('you are right')
+print(num)
+
+    
